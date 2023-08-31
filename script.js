583aceb12ac3f5f0fe8734eb7c1086ff4bba07c1
David Petr <d.petr.cz@gmail.com>
2023-08-31 16:04:40 -0700

    Add mouseover eventListener to each pixel creation

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//Create a 16 x 16 grid of div boxes using JS DOM manipulation
-
-//set the container to flex-wrap. May need to research how flex-wrap works
-//find the container width, divide by 16
-//Create a line of 256 divs, set each to 1/16th the width of container
-
 const container = document.querySelector('.gridContainer')
 const containerInfo = container.getBoundingClientRect();
 const containerWidth = containerInfo.width - 4;
@@ -16,8 +10,13 @@ function createPixel() {
   let pixel = document.createElement('div');
   pixel.classList.add('pixel');
   pixel.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`);
+  pixel.addEventListener('mouseover', hoverEvent);
 
   container.appendChild(pixel);
+}
+
+function hoverEvent() {
+  console.log('test');
 }
 
 //I thought it would be best to create each pixel div individually rather than trying to group them by row or something
