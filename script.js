5cf6fe20e860c69c514cc4e304a02a6f92d2fc5e
David Petr <d.petr.cz@gmail.com>
2023-08-31 11:25:13 -0700

    Create container div w/ border, set body to 100%

---
+++ script.js
@@ -0,0 +1,5 @@
+//Create a 16 x 16 grid of div boxes using JS DOM manipulation
+
+//set the container to flex-wrap. May need to research how flex-wrap works
+//find the container width, divide by 16
+//Create a line of 256 divs, set each to 1/16th the width of container
